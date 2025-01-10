a7d7eac0a217b7df81ae9493531f6415f0add314
Nguyễn Duy Hoàng <22520467@gm.uit.edu.vn>
2025-01-10 15:24:33 +0700

    Update ddpg_continuous_action.py

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -69,7 +69,7 @@ class Args:
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
-        if capture_video and record_bool and idx == 0:
+        if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", episode_trigger=lambda episode_id: episode_id % 4900 == 0)
         else:
